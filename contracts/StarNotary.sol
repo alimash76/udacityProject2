21518cfe87df6332434736c312d66ee959f5da62
alimash76 <alihejazi76@gmail.com>
2022-04-20 18:01:21 +0430

    It can now be compiled

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -5,7 +5,8 @@
 pragma solidity >=0.4.24;
 
 //Importing openzeppelin-solidity ERC-721 implemented Standard
-import "../app/node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//import "../app/node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../app/node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 // StarNotary Contract declaration inheritance the ERC721 openzeppelin implementation
 contract StarNotary is ERC721 {
